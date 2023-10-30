3a31384f17194f668b148016f0828967e2035c07
Kourdroid <kourdroid@gmail.com>
2023-10-30 11:46:58 +0100

    update

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,9 +1,7 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: [],
+  content: ["./src/**/*.html", "./src/**/*.js"],
   theme: {
-    extend: {},
-  },
-  plugins: [],
-}
 
+  },
+};
